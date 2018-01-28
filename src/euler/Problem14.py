c1eaedc47ba3fe23864ea3ef59d015d1eeb0d466
Marija Nedjalkova <nedyalkova.marya@gmail.com>
2018-01-28 17:10:39 +0000

    Optimized file to work with a dict. Execution time went down from 60 to 3 seconds.

--- src/euler/Problem14.py
+++ src/euler/Problem14.py
@@ -1,14 +1,32 @@
+import time
+
+result_dict = {} # starting_number: sequence_length
+
+
 def get_next_number(n):
     if n % 2 == 0:
         return n / 2
     return 3*n + 1
 
 
-def get_sequence(starting_number):
-    res = [starting_number]
+def get_sequence_length(starting_number):
+    first_number = starting_number
+    if starting_number in result_dict:
+        return result_dict[starting_number]
+    sequence = [starting_number]
+    already_seen = False
+    res = 0
     while starting_number > 1:
         starting_number = get_next_number(starting_number)
-        res.append(starting_number)
+        sequence.append(starting_number)
+        if starting_number in result_dict:
+            # append the number for that to the res
+            already_seen = True
+            res = len(sequence) - 1 + result_dict[starting_number]
+            break
+    if not already_seen:
+        res = len(sequence)
+    result_dict[first_number] = res
     return res
 
 
@@ -16,9 +34,7 @@ def get_starting_number_for_longest_chain():
     starting_number_for_longest_chain = 1
     max_length = 1
     for number in range(1, 1000000):
-        if number % 1000 == 0:
-            print(number)
-        new_length = len(get_sequence(number))
+        new_length = get_sequence_length(number)
         if new_length > max_length:
             max_length = new_length
             starting_number_for_longest_chain = number
@@ -26,4 +42,6 @@ def get_starting_number_for_longest_chain():
 
 
 if __name__=="__main__":
+    start_time = time.time()
     print("RES ", get_starting_number_for_longest_chain())
+    print("--- %s seconds ---" % (time.time() - start_time))
