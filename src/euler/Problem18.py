aaf6e134ce1a418b86049861a4d3d9c17724acf3
Marija Nedjalkova <nedyalkova.marya@gmail.com>
2018-02-08 22:00:51 +0000

    Optimised solution with a dictionary

--- src/euler/Problem18.py
+++ src/euler/Problem18.py
@@ -1,4 +1,6 @@
 from euler.input_Problem18 import input_string_15
+counter = 0
+sum_dict = {}
 
 
 def tests():
@@ -14,17 +16,32 @@ def assign_triangle(input_str):
 
 
 def find_max_sum(current_level=0, current_pos=0):
+    global counter
+    counter += 1
     current_num = triangle[current_level][current_pos]
+    if current_level in sum_dict and current_pos in sum_dict[current_level]:
+        return sum_dict[current_level][current_pos]
     if current_level + 1 == len(triangle):
         return max(triangle[current_level][current_pos:current_pos+2])
     left_sum = find_max_sum(current_level+1, current_pos)
     right_sum = find_max_sum(current_level+1, current_pos+1)
     if left_sum > right_sum:
-        return current_num + left_sum
-    return current_num + right_sum
+        to_add = left_sum
+    else:
+        to_add = right_sum
+    res = current_num + to_add
+    if current_level not in sum_dict:
+        sum_dict[current_level] = {}
+    sum_dict[current_level][current_pos] = res
+    return res
 
 
 if __name__ == "__main__":
     print(tests())
     assign_triangle(input_string_15)
+    counter = 0
+    sum_dict.clear()
+    if counter == 0 and not sum_dict.keys():
+        print("All data clear")
     print(find_max_sum())
+    print("Steps made: ", counter)
