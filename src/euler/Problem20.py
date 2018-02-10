a81324495ab6bd0cec4d46d86bfc2fd1924b0045
Marija Nedjalkova <nedyalkova.marya@gmail.com>
2018-02-10 22:30:31 +0000

    a very basic solution to pass the test

---
+++ src/euler/Problem20.py
@@ -0,0 +1,24 @@
+from math import factorial
+
+
+def tests():
+    res = find_factorial_digit_sum(10) == 27
+    return res
+
+
+def find_digit_sum(number):
+    res = 0
+    num = number
+    while num > 0:
+        res += num % 10
+        num = num // 10
+    return res
+
+
+def find_factorial_digit_sum(number):
+    fct = factorial(number)
+    return find_digit_sum(fct)
+
+
+if __name__ == "__main__":
+    print(tests())
